af3bd268289f94716541790909460a8ebf19c03b
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-01-01 15:54:40 -0800

    feat: add grid

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,53 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+const NUM_GRID_LINES: i64 = 10;
+const GRID_SIZE: f32 = 50.;
+
+const RED: Color = Color::hsl(0., 0.95, 0.7);
+const BLUE: Color = Color::hsl(200., 0.95, 0.7);
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2d);
 
-    // spawn a circle (using a sprite with circular appearance)
+    // spawn a circle
     commands.spawn((
-        Sprite {
-            color: Color::srgb(0.7, 0.3, 0.9),
-            custom_size: Some(Vec2::splat(50.0)),
-            ..default()
-        },
+        Mesh2d(meshes.add(Circle::new(0.4 * GRID_SIZE))),
+        MeshMaterial2d(materials.add(Color::WHITE)),
         Transform::from_xyz(0.0, 0.0, 0.0),
         Player,
     ));
+
+    // draw a grid
+    let horizontal_lines = (-NUM_GRID_LINES..NUM_GRID_LINES)
+        .map(|i| {
+            meshes.add(Polyline2d::new([
+                Vec2::new(-(NUM_GRID_LINES as f32) * GRID_SIZE, i as f32 * GRID_SIZE),
+                Vec2::new(NUM_GRID_LINES as f32 * GRID_SIZE, i as f32 * GRID_SIZE),
+            ]))
+        })
+        .collect::<Vec<_>>();
+
+    let vertical_lines = (-NUM_GRID_LINES..NUM_GRID_LINES)
+        .map(|i| {
+            meshes.add(Polyline2d::new([
+                Vec2::new(i as f32 * GRID_SIZE, -(NUM_GRID_LINES as f32) * GRID_SIZE),
+                Vec2::new(i as f32 * GRID_SIZE, NUM_GRID_LINES as f32 * GRID_SIZE),
+            ]))
+        })
+        .collect::<Vec<_>>();
+
+    for mesh in horizontal_lines {
+        commands.spawn((Mesh2d(mesh), MeshMaterial2d(materials.add(RED))));
+    }
+
+    for mesh in vertical_lines {
+        commands.spawn((Mesh2d(mesh), MeshMaterial2d(materials.add(BLUE))));
+    }
 }
 
 fn move_player(
