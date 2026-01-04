2e6ad5416c8cf6de7d61d98ccf52a4b3b5f35064
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-01-03 22:59:13 -0800

    feat: add walls, constrain movement to walls

--- src/main.rs
+++ src/main.rs
@@ -64,14 +64,16 @@ struct Vac {
 }
 
 impl Vac {
-    fn new() -> Self {
+    fn new(start: Vec2) -> Self {
         Self {
-            state: VacMovementState::Moving(Vec2::ZERO, Dir::East),
+            state: VacMovementState::Moving(start, Dir::East),
         }
     }
 }
 
 fn main() {
+    let map = Map::parse(MAP_STR);
+
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -82,6 +84,7 @@ fn main() {
             }),
             ..default()
         }))
+        .insert_resource(map)
         .add_systems(Startup, setup)
         .add_systems(Update, move_vac)
         .run();
@@ -89,35 +92,19 @@ fn main() {
 
 const NUM_GRID_LINES: i64 = 10;
 const GRID_SIZE: f32 = 50.;
+const STEP_TIME_MS: u64 = 500;
 
 const RED: Color = Color::hsl(0., 0.95, 0.7);
 const BLUE: Color = Color::hsl(200., 0.95, 0.7);
+const GRAY: Color = Color::hsl(0., 0.0, 0.3);
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    map: Res<Map>,
 ) {
     commands.spawn(Camera2d);
-
-    // spawn a circle with a triangle to show heading
-    commands.spawn((
-        Mesh2d(meshes.add(Circle::new(0.4 * GRID_SIZE))),
-        MeshMaterial2d(materials.add(Color::WHITE)),
-        Transform::from_xyz(0.0, 0.0, 0.1),
-        Vac::new(),
-        MovementTimer::new(),
-        // triangle
-        children![(
-            Mesh2d(meshes.add(Triangle2d::new(
-                Vec2::new(0., 0.2 * GRID_SIZE),
-                Vec2::new(0., -0.2 * GRID_SIZE),
-                Vec2::new(0.2 * GRID_SIZE, 0.),
-            ))),
-            MeshMaterial2d(materials.add(Color::BLACK)),
-            Transform::from_xyz(0.2 * GRID_SIZE, 0., 0.),
-        )],
-    ));
 
     // draw a grid
     let horizontal_lines = (-NUM_GRID_LINES..NUM_GRID_LINES)
@@ -145,6 +132,46 @@ fn setup(
     for mesh in vertical_lines {
         commands.spawn((Mesh2d(mesh), MeshMaterial2d(materials.add(BLUE))));
     }
+
+    // draw the map walls
+    let map_basept = Vec2::new(
+        -((map.width / 2) as f32) * GRID_SIZE,
+        -((map.height / 2) as f32) * GRID_SIZE,
+    );
+    for (x, y) in &map.walls {
+        let offset = Vec2::new(*x as f32 * GRID_SIZE, *y as f32 * GRID_SIZE);
+        let pos = map_basept + offset;
+
+        let wall = meshes.add(Rectangle::new(GRID_SIZE, GRID_SIZE));
+        commands.spawn((
+            Mesh2d(wall),
+            MeshMaterial2d(materials.add(GRAY)),
+            Transform::from_translation(pos.extend(0.1)),
+        ));
+    }
+
+    let (start_x, start_y) = &map.start;
+    let offset = Vec2::new(*start_x as f32 * GRID_SIZE, *start_y as f32 * GRID_SIZE);
+    let vac_start = map_basept + offset;
+
+    // spawn a circle with a triangle to show heading
+    commands.spawn((
+        Mesh2d(meshes.add(Circle::new(0.4 * GRID_SIZE))),
+        MeshMaterial2d(materials.add(Color::WHITE)),
+        Transform::from_xyz(0., 0., 0.1),
+        Vac::new(vac_start),
+        MovementTimer::new(),
+        // triangle
+        children![(
+            Mesh2d(meshes.add(Triangle2d::new(
+                Vec2::new(0., 0.2 * GRID_SIZE),
+                Vec2::new(0., -0.2 * GRID_SIZE),
+                Vec2::new(0.2 * GRID_SIZE, 0.),
+            ))),
+            MeshMaterial2d(materials.add(Color::BLACK)),
+            Transform::from_xyz(0.2 * GRID_SIZE, 0., 0.),
+        )],
+    ));
 }
 
 #[derive(Component, Deref, DerefMut)]
@@ -152,12 +179,16 @@ struct MovementTimer(Timer);
 
 impl MovementTimer {
     fn new() -> Self {
-        let timer = Timer::new(Duration::from_millis(1000), TimerMode::Repeating);
+        let timer = Timer::new(Duration::from_millis(STEP_TIME_MS), TimerMode::Repeating);
         Self(timer)
     }
 }
 
-fn move_vac(time: Res<Time>, mut query: Query<(&mut Transform, &mut Vac, &mut MovementTimer)>) {
+fn move_vac(
+    time: Res<Time>,
+    map: Res<Map>,
+    mut query: Query<(&mut Transform, &mut Vac, &mut MovementTimer)>,
+) {
     let (mut transform, mut vac, mut timer) = query.single_mut().unwrap();
     timer.tick(time.delta());
 
@@ -171,35 +202,133 @@ fn move_vac(time: Res<Time>, mut query: Query<(&mut Transform, &mut Vac, &mut Mo
                 let dest: Vec2 = base_pos + heading_vec * GRID_SIZE;
 
                 // finish moving to the destination point
-                transform.translation = dest.extend(0.0);
+                transform.translation = dest.extend(0.1);
 
-                // pick a direction to rotate in
-                vac.state = if rand::random_bool(0.5) {
-                    VacMovementState::Rotating(heading, heading.rotate_cw())
+                // if we aren't facing a wall, move forward.
+                // otherwise, rotate
+                let pos = transform.translation.xy();
+                let next_grid_pt = pos + Vec2::from(heading) * GRID_SIZE;
+
+                let map_basept = Vec2::new(
+                    -((map.width / 2) as f32) * GRID_SIZE,
+                    -((map.height / 2) as f32) * GRID_SIZE,
+                );
+
+                let facing_wall = map.walls.iter().any(|(x, y)| {
+                    let offset = Vec2::new(*x as f32 * GRID_SIZE, *y as f32 * GRID_SIZE);
+                    let wall_pos = map_basept + offset;
+
+                    wall_pos == next_grid_pt
+                });
+
+                if facing_wall {
+                    vac.state = if rand::random_bool(0.5) {
+                        VacMovementState::Rotating(heading, heading.rotate_cw())
+                    } else {
+                        VacMovementState::Rotating(heading, heading.rotate_ccw())
+                    };
                 } else {
-                    VacMovementState::Rotating(heading, heading.rotate_ccw())
-                };
+                    vac.state = VacMovementState::Moving(pos, heading);
+                }
             }
             VacMovementState::Rotating(_, end) => {
+                // if we aren't facing a wall, move forward.
+                // otherwise, keep rotating
                 let pos = transform.translation.xy();
-                vac.state = VacMovementState::Moving(pos, end);
+                let next_grid_pt = pos + Vec2::from(end) * GRID_SIZE;
+
+                let map_basept = Vec2::new(
+                    -((map.width / 2) as f32) * GRID_SIZE,
+                    -((map.height / 2) as f32) * GRID_SIZE,
+                );
+
+                let facing_wall = map.walls.iter().any(|(x, y)| {
+                    let offset = Vec2::new(*x as f32 * GRID_SIZE, *y as f32 * GRID_SIZE);
+                    let wall_pos = map_basept + offset;
+
+                    wall_pos == next_grid_pt
+                });
+
+                if facing_wall {
+                    vac.state = if rand::random_bool(0.5) {
+                        VacMovementState::Rotating(end, end.rotate_cw())
+                    } else {
+                        VacMovementState::Rotating(end, end.rotate_ccw())
+                    };
+                } else {
+                    vac.state = VacMovementState::Moving(pos, end);
+                }
             }
         }
     } else {
-        let elapsed = timer.elapsed_secs_f64() as f32;
+        let elapsed = timer.elapsed().as_millis() as f32 / STEP_TIME_MS as f32;
         match vac.state {
             VacMovementState::Moving(base_pos, heading) => {
                 let heading_vec: Vec2 = heading.into();
                 let dest: Vec2 = base_pos + heading_vec * GRID_SIZE;
 
                 let pos = Vec2::lerp(base_pos, dest, elapsed);
-                transform.translation = pos.extend(0.0);
+                transform.translation = pos.extend(0.1);
             }
             VacMovementState::Rotating(start, end) => {
                 let start = Quat::from_rotation_z(start.to_radians());
                 let end = Quat::from_rotation_z(end.to_radians());
                 transform.rotation = Quat::slerp(start, end, elapsed);
             }
+        }
+    }
+}
+
+const MAP_STR: &str = r"#######
+#...###
+#.#.###
+#S....#
+#.#.#.#
+#.#...#
+#.###.#
+#.....#
+#######
+";
+
+#[derive(Debug, Resource)]
+struct Map {
+    walls: Vec<(usize, usize)>,
+
+    // (x,y) pairs with y increasing upwards
+    start: (usize, usize),
+
+    width: usize,
+    height: usize,
+}
+
+impl Map {
+    pub fn parse(str: &str) -> Self {
+        let mut walls = Vec::new();
+        let mut start = (0, 0);
+
+        let width = str.lines().next().unwrap().len();
+        let height = str.lines().count();
+
+        for (row_idx, row) in str.lines().enumerate() {
+            for (col_idx, char) in row.chars().enumerate() {
+                match char {
+                    '#' => {
+                        walls.push((col_idx, height - row_idx - 1));
+                    }
+                    'S' => {
+                        start = (col_idx, height - row_idx - 1);
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        Self {
+            walls,
+            start,
+
+            width,
+            height,
         }
     }
 }
